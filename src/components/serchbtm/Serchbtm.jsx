c6d91d44d26604019d2303d17cdfc9136aece6e1
salohuddinov <salohuddinov@gmail.com>
2024-05-13 08:30:28 +0300

    muhammadal1

--- src/components/serchbtm/Serchbtm.jsx
+++ src/components/serchbtm/Serchbtm.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import './Serchbtm.css'
+import { Link } from 'react-router-dom'
 
 const Serchbtm = ({ data }) => {
     let serchItem = data?.map(el =>
         <div className='serch__item' key={el.id}>
-            <h2>{el.title}</h2>
+            <Link to={`/single/${el.id}`}><h2>{el.title}</h2></Link>
             <img src={el.thumbnail} alt={el.title} />
         </div>)
     return (
